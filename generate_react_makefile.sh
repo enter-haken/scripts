c6ebf8aeb31a9057cc0d68cc0d582d39e8bc6717
Jan Frederik Hake <jan_hake@gmx.de>
2018-10-10 11:33:14 +0200

    added generate_react_makefile.sh

---
+++ generate_react_makefile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cat <<EOT >Makefile
+PACKAGE_MANAGER = 'yarn'
+
+.PHONY: default
+default: build
+
+.PHONY: build
+build:
+	if [ ! -d ./node_modules/ ]; then \$(PACKAGE_MANAGER) install; fi
+	\$(PACKAGE_MANAGER) run build
+
+.PHONY: clean
+clean:
+	rm build/ -rf
+
+.PHONY: clean_node_modules 
+clean_node_modules:
+	rm node_modules/ -rf
+
+.PHONY: deep_clean
+deep_clean: clean clean_node_modules
+
+.PHONY: run
+run: 
+	\$(PACKAGE_MANAGER) run start
+EOT
+
