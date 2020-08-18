7c213a5071659d70ac86c84089eea38039cdd92f
Jan Frederik Hake <jan_hake@gmx.de>
2020-08-18 14:15:31 +0200

    changed remember script behaviour

--- remember.sh
+++ remember.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-  key="$1"
-
-  case $key in
-    -s|--search)
-      SEARCH="$2"
-      shift 2 # shift to next parameter
-      ;;
-  esac
-done
-set -- "$POSITIONAL[@]"
-
-if [ ! -z "$SEARCH" ]; then
-  brain --search $SEARCH | fdp -Tpng | feh -Z -
+if [ ! -z "$1" ]; then
+  brain --search $1 | fdp -Tpng | feh -Z -
 else
   brain --all | fdp -Tpng | feh -Z -
 fi
