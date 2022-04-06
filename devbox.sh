30e01b62e35cd0b9367bdfa7a11dd0240324dd6f
Jan Frederik Hake <jan_hake@gmx.de>
2022-04-06 11:28:08 +0200

    fixed devbox

--- devbox.sh
+++ devbox.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-if [ "$( docker container inspect -f '{{.State.Status}}' devbox)" != "running" ]; then
-  make -C ~/.config/nvim/devbox/ 
-else
-  make -C ~/.config/nvim/devbox/ down
-fi
+if [ "$1" == "down" ]; then
+  echo "stoping devbox container";
+  if [ ! "$(docker ps -q -f name=devbox)" ] || [ "$(docker container inspect -f '{{.State.Status}}' devbox)" == "running" ]; then
+    make -C ~/.config/nvim/devbox/ down 
+  fi;
+  exit 0;
+fi;
+
+if [ ! "$(docker ps -q -f name=devbox)" ] || [ "$( docker container inspect -f '{{.State.Status}}' devbox)" != "running" ]; then
+  make -C ~/.config/nvim/devbox/ build
+fi;
+
+make -C ~/.config/nvim/devbox/ exec
